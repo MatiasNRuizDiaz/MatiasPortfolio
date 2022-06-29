15246110a9d8f4e7fdb475871646a6d6059690fc
Matias Ruiz Diaz <73109277+MatiasNRuizDiaz@users.noreply.github.com>
2022-06-29 20:03:47 -0300

    cambios

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ navToggle.addEventListener("click", () => {
   navMenu.classList.toggle("nav-menu_visible");
 
   if (navMenu.classList.contains("nav-menu_visible")) {
-    navToggle.setAttribute("aria-label", "Cerrar menú");
-  } else {
     navToggle.setAttribute("aria-label", "Abrir menú");
+  } 
+  else {
+    navToggle.setAttribute("aria-label", "Cerrar menú");
   }
 });
